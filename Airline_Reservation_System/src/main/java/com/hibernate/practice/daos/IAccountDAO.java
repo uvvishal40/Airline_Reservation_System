bdfc63ee778b4fcf7679be6838dc1e6460fa62b8
uvvishal40 <uppinvishal76@gmail.com>
2021-02-09 12:49:34 +0530

    filedeleted

--- Airline_Reservation_System/src/main/java/com/hibernate/practice/daos/IAccountDAO.java
+++
@@ -1,9 +0,0 @@
-package com.hibernate.practice.daos;
-
-import com.hibernate.practice.entities.Account;
-
-public interface IAccountDAO {
-    boolean register(Account account);
-    Account login(Account account);
-    boolean forgotPassword(Account account);
-}
